20bb35359027237d2046faba6ae0f0ffd04c6fc5
Libor Pecinka <libor@lipe.cz>
2022-11-05 15:46:28 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const App: FC = (): JSX.Element => {
+  return <></>;
+};
+
+export default App;
