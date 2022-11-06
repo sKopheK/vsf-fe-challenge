8a2a5f6bd33eb84aedf9500465d021f0661449f5
sKopheK <libor@lipe.cz>
2022-11-06 19:56:15 +0100

    rapid API key added

--- src/apiKeys.example.ts
+++ src/apiKeys.example.ts
@@ -1,2 +1,3 @@
 export const OPEN_WEATHER_MAP = '';
-export const IP_DATA = ''; // used for fallback when getlocation is not available in browser
+export const IP_DATA = '';
+export const RAPID = '';
