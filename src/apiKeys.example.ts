ebfa7ed1062fc15cdd45359b456f11e51d3e50ef
sKopheK <libor@lipe.cz>
2022-11-06 19:54:35 +0100

    IpData API key constant rename

--- src/apiKeys.example.ts
+++ src/apiKeys.example.ts
@@ -1,2 +1,2 @@
 export const OPEN_WEATHER_MAP = '';
-export const IPDATA = ''; // used for fallback when getlocation is not available in browser
+export const IP_DATA = ''; // used for fallback when getlocation is not available in browser
